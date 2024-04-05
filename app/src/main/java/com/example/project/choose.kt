cd640303a3eb4b4a9978ae2a0e52ae506fa9aba5
seongbin <czy105211@gmail.com>
2024-04-05 17:03:47 +0900

    칵테일 선택 기능 UI업데이트(완료)

--- app/src/main/java/com/example/project/choose.kt
+++ app/src/main/java/com/example/project/choose.kt
@@ -1,39 +1,56 @@
 package com.example.project
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import java.io.BufferedReader
-import java.io.File
-import java.io.FileReader
 
 class choose : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_choose)
+
+        val recyclerView: RecyclerView = findViewById(R.id.cocktail_list)
+        val cock_name = listOf(
+            "matini",
+            "ginfizz",
+            "gintonic",
+            "orangeblossom",
+            "paradise",
+            "tomcollins",
+            "whitelady"
+        )
+        val adapter = CocktaillistAdapter(cock_name)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btn7 = findViewById<Button>(R.id.button7)
+        val btn7 = findViewById<Button>(R.id.btn_back)
         btn7.setOnClickListener {
             finish()
         }
-        val btn = findViewById<Button>(R.id.button)
-        btn.setOnClickListener {
-            val fileContent = readTextFile(this, R.raw.a)
-            val intent = Intent(this, select::class.java).apply {
-                putExtra("fileContent", fileContent)
-            }
-            startActivity(intent)
-        }
+
+
+
+//        val btn = findViewById<Button>(R.id.button)
+//        btn.setOnClickListener {
+//            val fileContent = readTextFile(this, R.raw.a)
+//            val intent = Intent(this, select::class.java).apply {
+//                putExtra("fileContent", fileContent)
+//            }
+//            startActivity(intent)
+//        }
     }
 }
 
